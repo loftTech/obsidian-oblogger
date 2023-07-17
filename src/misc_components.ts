ecbf6b4e3890342a75279588aa84af0c71961c44
brad <robotbrad@me.com>
2023-07-17 12:19:41 -0700

    removed unused code as well as corresponding css

--- src/misc_components.ts
+++ src/misc_components.ts
@@ -7,21 +7,13 @@ export function buildSeparator(
     separatorContainer.addClass("separator-container");
     separatorContainer.addClass(className);
 
-    const separatorBorderLineLeftDiv = document.createElement("div");
-    separatorBorderLineLeftDiv.addClass("separator-border-line-left");
-
     // metadata separator div
     const metadataSeparatorDiv = document.createElement("div");
     metadataSeparatorDiv.addClass("separator");
     metadataSeparatorDiv.setText(text);
     metadataSeparatorDiv.ariaLabel = tooltipText;
 
-    const separatorBorderLineRightDiv = document.createElement("div");
-    separatorBorderLineRightDiv.addClass("separator-border-line-right");
-
-    separatorContainer.appendChild(separatorBorderLineLeftDiv);
     separatorContainer.appendChild(metadataSeparatorDiv);
-    separatorContainer.appendChild(separatorBorderLineRightDiv);
 
     return separatorContainer;
 }
