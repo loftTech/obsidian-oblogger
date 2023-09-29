81ce5bca2c5a1b718d9c31f1233ff27ee4392ae6
Matt Phair <matt.phair@icloud.com>
2023-09-28 19:53:23 -0700

    add first impelmentation of folder container

---
+++ src/containers/otc/folder_container.ts
@@ -0,0 +1,86 @@
+import { OtcContainer } from "./otc_container";
+import { ContainerCallbacks } from "../container_callbacks";
+import { ObloggerSettings, OtcGroupType } from "../../settings";
+import { App, TFile, TFolder } from "obsidian";
+import { FileState } from "../../constants";
+
+export class FolderContainer extends OtcContainer {
+    basePath: string;
+
+    constructor(
+        app: App,
+        settings: ObloggerSettings,
+        callbacks: ContainerCallbacks,
+        basePath: string,
+        isPinned: boolean
+    ) {
+        super(
+            app,
+            settings,
+            callbacks,
+            OtcGroupType.FOLDER_GROUP,
+            basePath, // groupName,
+            isPinned
+        );
+
+        this.basePath = basePath;
+    }
+
+    private addAllFilesIn(folder: TFolder, excludedFolders: string[]): void {
+        folder.children.forEach(abstractFile => {
+            if (abstractFile instanceof TFile) {
+                if (this.isFileExcluded(abstractFile, excludedFolders)) {
+                    return;
+                }
+                this.addFileToFolder(
+                    abstractFile,
+                    "",
+                    folder.path);
+            } else if (abstractFile instanceof TFolder) {
+                this.addAllFilesIn(abstractFile, excludedFolders);
+            }
+        })
+    }
+
+    protected buildFileStructure(excludedFolders: string[]): void {
+        const abstractFile = this.app.vault.getAbstractFileByPath(this.basePath);
+        if (!(abstractFile instanceof TFolder)) {
+            console.warn(`Unable to build container for ${this.basePath}. It is not a folder.`)
+            return;
+        }
+        this.addAllFilesIn(abstractFile, excludedFolders);
+    }
+
+    protected getPillClickHandler(): ((e: MouseEvent) => void) | undefined {
+        return undefined;
+        // todo
+    }
+
+    protected getTextIcon(): string {
+        return "folder";
+        // todo
+    }
+
+    protected getTextIconTooltip(): string {
+        return "";
+    }
+
+    protected getTitleText(): string {
+        return this.basePath.split("/").last() ?? this.basePath;
+    }
+
+    protected getTitleTooltip(): string {
+        return "";
+    }
+
+    protected wouldBeRendered(state: FileState): boolean {
+        let currentFolder = state.file.parent;
+        while (currentFolder) {
+            if (currentFolder.path === this.basePath) {
+                return true;
+            }
+            currentFolder = currentFolder.parent;
+        }
+        return false;
+    }
+}
